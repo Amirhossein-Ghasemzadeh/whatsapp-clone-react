1d04590e41121efae4aa6f120834c5440801e6c7
Amirhossein_Ghasemzadeh <amirghasemzadeh17@gmail.com>
2021-11-02 20:20:01 +0330

    one

---
+++ src/Chat.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
+
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined, AttachFile } from "@material-ui/icons";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const Chat = () => {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg `} />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className={`chat__message && ${true && "chat__reciever"}`}>
+          <span className="chat__name">sonyyy</span>Hey guys
+          <span className="chat__timestamp">3:52pm</span>
+        </p>
+      </div>
+
+      <div className="chat__footer"></div>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBk1K5WtWki-Q2TevxGTDfPDVNHfxTPe9M",
+  authDomain: "whatsapp-clone-be544.firebaseapp.com",
+  projectId: "whatsapp-clone-be544",
+  storageBucket: "whatsapp-clone-be544.appspot.com",
+  messagingSenderId: "632002408969",
+  appId: "1:632002408969:web:5ab86863fb6beb54cc3ed8",
+  measurementId: "G-Z3WFWBSFDM",
+};
